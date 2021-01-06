6383aca8301847e465aebfebbc9e58a439e1897f
punkrou404 <punkrou404@gmail.com>
2021-01-06 19:51:56 +0900

    ENHANCE: update Classes

--- src/service/GoogleAppsScriptAPI/DocumentAppW.js
+++ src/service/GoogleAppsScriptAPI/DocumentAppW.js
@@ -1,45 +1,48 @@
-/**
- * @func 段落内の文章を取得する
- * @param {string} title - 段落名
- * @param {string} fileId - ファイルID
- */
-const getParagraphContent = (title, fileId) => {
-    console.log(`DocumentAppW#getParagraphContent start`);
+class DocumentAppW {
+    /**
+     * @func 段落内の文章を取得する
+     * @param {string} title - 段落名
+     * @param {string} fileId - ファイルID
+     */
+    getParagraphContent(title, fileId) {
+        console.log(`DocumentAppW#getParagraphContent start`);
 
-    let active = false;
-    let content = '';
+        let active = false;
+        let content = '';
 
-    DocumentApp.openById(fileId)
-    .getBody()
-    .getParagraphs()
-    .forEach(e => {
-        // お目当ての段落だった為、フラグを立てて次へ
-        if(title === e.getText()) {
-            active = true;
-            return;
-        }
+        DocumentApp.openById(fileId)
+        .getBody()
+        .getParagraphs()
+        .forEach(e => {
+            // お目当ての段落だった為、フラグを立てて次へ
+            if(title === e.getText()) {
+                active = true;
+                return;
+            }
 
-        // お目当ての段落が終わった為、フラグを回収して次へ
-        if(DocumentApp.ParagraphHeading.HEADING1 === e.getHeading()) {
-            active = false;
-            return;
-        }
+            // お目当ての段落が終わった為、フラグを回収して次へ
+            if(DocumentApp.ParagraphHeading.HEADING1 === e.getHeading()) {
+                active = false;
+                return;
+            }
 
-        // お目当ての段落内のテキストは保持しておく
-        if(active) {
-            content = e.getText();
-        }
-    });
+            // お目当ての段落内のテキストは保持しておく
+            if(active) {
+                content = e.getText();
+            }
+        });
 
-    return content;
-};
+        return content;
+    };
 
-/**
- * @func ファイル内の文章を全て取得する
- * @param {string} fileId - ファイルID 
- */
-const getContent = (fileId) => {
-    console.log(`DocumentAppW#getContent start`);
+    /**
+     * @func ファイル内の文章を全て取得する
+     * @param {string} fileId - ファイルID 
+     */
+    getContent(fileId) {
+        console.log(`DocumentAppW#getContent start`);
 
-    return DocumentApp.openById(fileId).getBody().getText();
-};
+        return DocumentApp.openById(fileId).getBody().getText();
+    };
+
+}
--- src/service/GoogleChatsWebhook.js
+++ src/service/GoogleChatsWebhook.js
@@ -1,23 +1,26 @@
-/**
- * @func GoogleChatsに投稿する
- * @param {string} text - 投稿内容
- * @param {string} webhookURL - 投稿先のGoogleChatsの部屋のwebhookURL
- */
-const post = (text, webhookURL) => {
-  console.log(`post start`);
-
-  const payload = JSON.stringify({
-    'text' : text,
-  });
+class GoogleChatsWebhook{
+  /**
+   * @func GoogleChatsに投稿する
+   * @param {string} text - 投稿内容
+   * @param {string} webhookURL - 投稿先のGoogleChatsの部屋のwebhookURL
+   */
+  post(text, webhookURL) {
+    console.log(`post start`);
   
-  try {
-    UrlFetchApp.fetch(webhookURL, {
-      'method' : 'POST',
-      'contentType' : 'application/json; charset=utf-8',
-      'payload' : payload
+    const payload = JSON.stringify({
+      'text' : text,
     });
-  } catch(e) {
-    console.error(e);
-    throw new Error(`WebhookURLが変更されてないか確認してください。現在：${webhookURL}`);
-  }
-};
+    
+    try {
+      UrlFetchApp.fetch(webhookURL, {
+        'method' : 'POST',
+        'contentType' : 'application/json; charset=utf-8',
+        'payload' : payload
+      });
+    } catch(e) {
+      console.error(e);
+      throw new Error(`WebhookURLが変更されてないか確認してください。現在：${webhookURL}`);
+    }
+  };
+}
+
